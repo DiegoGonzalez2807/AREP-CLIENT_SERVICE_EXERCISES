569a9603d3cecfe28a616edceb1ef20487b65cac
diego gonzalez <diego.gonzalez-g@mail.escuelaing.edu.co>
2022-09-23 21:06:46 -0500

    Ahora si, ultimo despliegue

--- resources/public/js/app.js
+++ resources/public/js/app.js
@@ -1,7 +1,7 @@
 function traerPerrito(){
-    window.location.href = "http://localhost:4567/prueba.png";
+    window.location.href = "https://salty-fjord-82376.herokuapp.com//prueba.png";
 }
 
 function traerHTML(){
-    window.location.href = "http://localhost:4567/pruebaIndex.html";
+    window.location.href = "https://salty-fjord-82376.herokuapp.com//pruebaIndex.html";
 }
